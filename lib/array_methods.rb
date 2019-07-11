cc2f1eff26766015535e0be7176089092d07f13e
Aaron Wilson <codingsensei19@gmail.com>
2019-07-11 23:27:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,8 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  # Add ya = ["A", "B", "C"]
+a.find_index {|item| item.casecmp("b") == 0 }
+# or
+a.find_index {|item| item.downcase == "b" }our solution here
 end
 
 def find_max_value(array)
