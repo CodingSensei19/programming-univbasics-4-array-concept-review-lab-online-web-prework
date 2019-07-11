1f949e1c7362862dc528aac93c6aa0d2583324c2
Aaron Wilson <codingsensei19@gmail.com>
2019-07-11 23:29:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,7 +6,10 @@ a.find_index {|item| item.downcase == "b" }our solution here
 end
 
 def find_max_value(array)
-  # Add your solution here
+  # Add yo[3, 5, 11, 16].max                                # => 16
+["three", "five", "eleven", "sixteen"].max        # => "three"
+["three", "five", "eleven", "sixteen"].max { |x,y| x.size <=> y.size }
+# => "sixteen"ur solution here
 end
 
 def find_min_value(array)
