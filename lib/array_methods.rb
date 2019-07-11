6c956cff76a329d819f6c8c1b8f4c40e3645e116
Aaron Wilson <codingsensei19@gmail.com>
2019-07-11 23:31:55 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,17 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  # Add ya = ["A", "B", "C"]
+a.find_index {|item| item.casecmp("b") == 0 }
+# or
+a.find_index {|item| item.downcase == "b" }our solution here
 end
 
 def find_max_value(array)
-  # Add your solution here
+  # Add yo[3, 5, 11, 16].max                                # => 16
+["three", "five", "eleven", "sixteen"].max        # => "three"
+["three", "five", "eleven", "sixteen"].max { |x,y| x.size <=> y.size }
+# => "sixteen"ur solution here
 end
 
 def find_min_value(array)
-  # Add your solution here
+[5,10].min
 end
